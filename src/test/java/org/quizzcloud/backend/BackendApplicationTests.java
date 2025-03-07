f87e4467422ef1bfcab866963288aae77fe28522
Sergi Virgili <svirgilif@gmail.com>
2025-03-07 13:26:10 +0100

    chore: init repository

---
+++ src/test/java/org/quizzcloud/backend/BackendApplicationTests.java
@@ -0,0 +1,14 @@
+package org.quizzcloud.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Test if the context loads
+	}
+
+}
